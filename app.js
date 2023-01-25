bedb0c9925242060538f14192a543138a8a8b01f
ALEJANDRO RAFAEL LOPEZ <alejandroxrxlopez@gmail.com>
2023-01-25 17:23:15 -0500

    sign up linked

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const signInForm = document.getElementById('sign-in');
 
 const signUpForm = document.getElementById('sign-up');
 
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(signUpForm);
+
+    await signupUser(data.get('email'), data.get('password'));
+});
 // Wire up sign in and sign up forms to supabase
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
